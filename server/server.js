4cee95b04bee4d59926fccc1bffedc4149c15cf6
Attaullah Ansari <mdattaullahansari152@gmail.com>
2025-12-20 22:12:16 +0530

    Backend: Setup MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected Successfully");
+  } catch (err) {
+    console.error("MongoDB Connection Error:", err);
+    process.exit(1);
+  }
+};
 
 app.get("/", (req, res) => {
-  res.send("server is running")
-})
+  res.send("Backend is Running!");
+});
 
-app.listen(PORT, () => {
-  console.log(`server is running on ${PORT}`)
-})
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+});
