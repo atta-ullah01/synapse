806fec3f037532fe694748b4cfbf150bdaf6bd71
Attaullah Ansari <mdattaullahansari152@gmail.com>
2025-12-21 12:59:42 +0530

    Auth: Implement Login and Signup API with JWT

--- server/models/User.js
+++ server/models/User.js
@@ -7,15 +7,15 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true }
 });
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-return mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
+
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const authRoutes = require("./routes/auth");
 require("dotenv").config();
 
 const app = express();
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 const connectDB = async () => {
   try {
