4e265920541a0a6542674b9561ffe6bb8ed0778b
Attaullah Ansari <mdattaullahansari152@gmail.com>
2025-12-22 17:36:16 +0530

    Socket: Implement real-time code synchronization

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+require("dotenv").config();
+
 const authRoutes = require("./routes/auth");
 const roomRoutes = require("./routes/rooms.js");
-require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/rooms/', roomRoutes);
 
@@ -23,12 +27,33 @@ const connectDB = async () => {
   }
 };
 
-app.get("/", (req, res) => {
-  res.send("Backend is Running!");
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("join-room", (roomId) => {
+        socket.join(roomId);
+        console.log(`User ${socket.id} joined room: ${roomId}`);
+    });
+
+    socket.on("code-change", ({ roomId, filename, code }) => {
+        socket.to(roomId).emit("code-update", { filename, code });
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
 });
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
 });
